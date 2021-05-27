b413fcd3cab3f8ad31387b6bb716b805835bbec9
Zou Wei <zwcloud@hotmail.com>
2021-05-27 13:37:06 +0800

    Fixed functions

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -582,6 +582,18 @@ private:
         fragShaderStageInfo.pName = "main";
         VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
 
+    	//Pipeline layout
+    	VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+    	pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    	pipelineLayoutInfo.setLayoutCount = 0;
+    	pipelineLayoutInfo.pSetLayouts = nullptr;
+    	pipelineLayoutInfo.pushConstantRangeCount = 0;
+    	pipelineLayoutInfo.pPushConstantRanges = nullptr;
+
+    	if(vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS)
+    	{
+    		throw std::runtime_error("failed to create pipeline layout!");
+    	}
 
         vkDestroyShaderModule(device, fragShaderModule, nullptr);
         vkDestroyShaderModule(device, vertShaderModule, nullptr);
@@ -605,6 +617,109 @@ private:
     }
 #pragma endregion
 
+#pragma region Fixed functions
+    //Vertex input
+	void createVertexInput()
+    {
+		VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+		vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    	vertexInputInfo.vertexBindingDescriptionCount = 0;
+    	vertexInputInfo.pVertexBindingDescriptions = nullptr;
+    	vertexInputInfo.vertexAttributeDescriptionCount = 0;
+    	vertexInputInfo.pVertexAttributeDescriptions = nullptr;
+    }
+	//Input assembly
+    void createInputAssembly()
+    {
+	    VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
+        inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        inputAssembly.primitiveRestartEnable = VK_FALSE;
+    }
+    //Viewports and scissors
+    void createViewportAndScissor()
+    {
+        VkViewport viewport{};
+    	viewport.x = 0.0f;
+    	viewport.y = 0.0f;
+    	viewport.width = (float) swapChainExtent.width;
+    	viewport.height = (float) swapChainExtent.height;
+    	viewport.minDepth = 0.0f;
+    	viewport.maxDepth = 1.0f;
+
+    	VkRect2D scissor{};
+    	scissor.offset = {0, 0};
+    	scissor.extent = swapChainExtent;
+
+    	VkPipelineViewportStateCreateInfo viewportState{};
+    	viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    	viewportState.viewportCount = 1;
+    	viewportState.pViewports = &viewport;
+    	viewportState.scissorCount = 1;
+    	viewportState.pScissors = &scissor;
+    }
+	//Rasterizer
+	void createRasterizer()
+    {
+	    VkPipelineRasterizationStateCreateInfo rasterizer{};
+    	rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    	rasterizer.depthClampEnable = VK_FALSE;
+    	rasterizer.rasterizerDiscardEnable = VK_FALSE;
+    	rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+    	rasterizer.lineWidth = 1.0f;
+    	rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    	rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    	rasterizer.depthBiasEnable = VK_FALSE;
+    	rasterizer.depthBiasConstantFactor = 0.0f;
+    	rasterizer.depthBiasClamp = 0.0f;
+    	rasterizer.depthBiasSlopeFactor = 0.0f;
+    }
+    //Multisampling
+	void createMultisampling()
+    {
+    	VkPipelineMultisampleStateCreateInfo multisampling{};
+    	multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    	multisampling.sampleShadingEnable = VK_FALSE;
+		multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+		multisampling.minSampleShading = 1.0f;
+		multisampling.pSampleMask = nullptr;
+		multisampling.alphaToCoverageEnable = VK_FALSE;
+		multisampling.alphaToOneEnable = VK_FALSE;
+    }
+	//Depth and stencil testing (nullptr for now)
+	//Color blending
+	void createColorBlending()
+    {
+	    VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+    	colorBlendAttachment.colorWriteMask =
+            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT
+    		| VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    	colorBlendAttachment.blendEnable = VK_FALSE;
+    	colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
+		colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;
+		colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+		colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+		colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+		colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+    }
+	//Dynamic state
+	void createDynamicState()
+    {
+	    VkDynamicState dynamicStates[] =
+        {
+        	VK_DYNAMIC_STATE_VIEWPORT,
+        	VK_DYNAMIC_STATE_LINE_WIDTH
+        };
+
+    	VkPipelineDynamicStateCreateInfo dynamicState{};
+    	dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+    	dynamicState.dynamicStateCount = 2;
+    	dynamicState.pDynamicStates = dynamicStates;
+    }
+	//Pipeline layout
+    VkPipelineLayout pipelineLayout;
+	
+#pragma endregion 
     void initVulkan()
     {
         printExtensions();
@@ -628,6 +743,8 @@ private:
 
     void cleanup()
     {
+        vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+    	
 	    for (VkImageView imageView : swapChainImageViews)
 	    {
 		    vkDestroyImageView(device, imageView, nullptr);
