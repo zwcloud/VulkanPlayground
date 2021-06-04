e1a3910b36e36f91e9f37e56050227c732fc45d4
Zou Wei <zwcloud@hotmail.com>
2021-06-04 13:18:26 +0800

    Framebuffers

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -803,6 +803,31 @@ private:
         vkDestroyShaderModule(device, vertShaderModule, nullptr);
     }
 
+#pragma region Framebuffers
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+    void createFramebuffers()
+    {
+    	swapChainFramebuffers.resize(swapChainImageViews.size());
+        for (size_t i = 0; i < swapChainImageViews.size(); ++i)
+        {
+	        VkImageView attachments[] = { swapChainImageViews[i] };
+
+        	VkFramebufferCreateInfo framebufferInfo{};
+        	framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        	framebufferInfo.renderPass = renderPass;
+        	framebufferInfo.attachmentCount = 1;
+        	framebufferInfo.pAttachments = attachments;
+        	framebufferInfo.width = swapChainExtent.width;
+        	framebufferInfo.height = swapChainExtent.height;
+        	framebufferInfo.layers = 1;
+        	if(vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS)
+        	{
+        		throw std::runtime_error("failed to create framebuffer!");
+        	}
+        }
+    }
+#pragma endregion
+
     void initVulkan()
     {
         printExtensions();
@@ -815,6 +840,7 @@ private:
         createImageviews();
         createRenderPass();
         createGraphicsPipeline();
+    	createFramebuffers();
     }
 
     void mainLoop()
@@ -827,6 +853,10 @@ private:
 
     void cleanup()
     {
+	    for (auto framebuffer : swapChainFramebuffers)
+	    {
+		    vkDestroyFramebuffer(device, framebuffer, nullptr);
+	    }
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
